e33e0e4428c8ce4e21541e2aeb3d280cb970a0d1
cdxofgithub <cdxofemail@163.com>
2017-06-27 13:28:26 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
